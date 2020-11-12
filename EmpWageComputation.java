193f5c81095aaa55f61ba81d65d06392105fd0f6
unknown <miyashaheen1995@gmail.com>
2020-11-13 00:10:36 +0530

    Added : Code to display Welcome Message

---
+++ EmpWageComputation.java
@@ -0,0 +1,9 @@
+package employeeWageAssignmentProblem;
+
+public class EmpWageComputation {
+	public static void main(String []args) {
+
+		System.out.println("Welcome to Employee Wage Computation Program !");
+	}
+
+}
