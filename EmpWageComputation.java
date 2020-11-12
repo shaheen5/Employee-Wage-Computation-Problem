5067f51be50c860010fdf455fc52f4b5e2606206
unknown <miyashaheen1995@gmail.com>
2020-11-13 01:24:28 +0530

    Modified : Code to add and calculate part time employee wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,14 +5,17 @@ public class EmpWageComputation {
 
 		System.out.println("Welcome to Employee Wage Computation Program !");
 		//Constants
-		int IS_FULL_TIME=1;
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
 		int EMP_RATE_PER_HOUR=20;
 		//variables
 		int empWorkHrs=0;
 		int empWage=0;
 		//use random function to generate 0 and 1
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if (empCheck == IS_FULL_TIME)
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if (empCheck == IS_PART_TIME)
+			empWorkHrs=4;
+		else if (empCheck == IS_FULL_TIME)
 			empWorkHrs=8;
 		else
 			empWorkHrs=0;
