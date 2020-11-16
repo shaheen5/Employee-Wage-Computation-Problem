ceb4a9cbea8c72ea1bbe02b4079b15645ffd5df3
unknown <miyashaheen1995@gmail.com>
2020-11-16 11:43:34 +0530

    Refactor : changed if else to switch case & used static constants

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,24 +1,25 @@
 package employeeWageAssignmentProblem;
 
 public class EmpWageComputation {
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+
 	public static void main(String []args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program !");
-		//Constants
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int EMP_RATE_PER_HOUR=20;
 		//variables
 		int empWorkHrs=0;
 		int empWage=0;
-		//use random function to generate 0 and 1
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if (empCheck == IS_PART_TIME)
-			empWorkHrs=4;
-		else if (empCheck == IS_FULL_TIME)
-			empWorkHrs=8;
-		else
-			empWorkHrs=0;
+		//computation
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		switch (empCheck) {
+			case IS_PART_TIME: empWorkHrs=4;
+				           break;
+			case IS_FULL_TIME: empWorkHrs=8;
+	                   		   break;
+			default : empWorkHrs=0;
+		}
 		empWage=empWorkHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage :- "+empWage);
 
